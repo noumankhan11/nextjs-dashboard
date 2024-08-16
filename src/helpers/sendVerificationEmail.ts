b8b341532a26479ab931db8f6408d9d4523f72b3
Nouman Khan <nomiofficial68@gmail.com>
2024-08-16 18:00:20 +0500

    setup for message email using resend and react email

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,30 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: email,
+      subject: "Mystry message | Verification code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+
+    return {
+      success: true,
+      message: "Verification email sent successfully",
+    };
+  } catch (error) {
+    console.error("Error sending verification email error: ", error);
+    return {
+      success: false,
+      message: "failed to send verification email",
+    };
+  }
+}
---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to the databse");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(
+      process.env.MONGO_URI || "",
+      {}
+    );
+
+    connection.isConnected = db.connections[0].readyState;
+    // TODO: log db and db.connection
+  } catch (error) {
+    console.log("database connection failed error:", error);
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
