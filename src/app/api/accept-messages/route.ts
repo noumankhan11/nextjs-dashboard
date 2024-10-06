0fe5ed780ef25b69b72066b5006590946a089086
Nouman Khan <nomiofficial68@gmail.com>
2024-10-06 17:36:58 +0500

    check the acceptingMessage status of a user and update it

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,116 @@
+import dbConnect from "@/lib/dbConnect";
+import { User, getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import UserModel from "@/model/User.model";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  // getServerSession allows you to access the user's session information on the server side,
+  const session = await getServerSession(authOptions);
+  // getting the user that i have injected in session in AuthOption's callback;
+  const user: User = session?.user as User;
+
+  if (!session || !session.user) {
+    return Response.json(
+      { success: false, message: "Not authenticated" },
+      { status: 401 }
+    );
+  }
+  const userId = user._id;
+  const { acceptMessages } = await request.json();
+
+  try {
+    // Update the user's message acceptance status
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      { isAcceptingMessages: acceptMessages },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      // User not found
+      return Response.json(
+        {
+          success: false,
+          message:
+            "Unable to find user to update message acceptance status",
+        },
+        { status: 404 }
+      );
+    }
+
+    // Successfully updated message acceptance status
+    return Response.json(
+      {
+        success: true,
+        message: "Message acceptance status updated successfully",
+        updatedUser,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error updating message acceptance status:", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error updating message acceptance status",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+// checking user messages acceptance status by a git method
+
+export async function GET(request: Request) {
+  await dbConnect;
+
+  // Get the user session
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  // check if the user is authenticated
+  if (!session || !user) {
+    return Response.json(
+      {
+        success: false,
+        message: "You must be logged in to view your messages",
+      },
+      { status: 401 }
+    );
+  }
+
+  try {
+    // Retrieve the user from the database using the ID
+    const foundUser = await UserModel.findById(user._id);
+
+    if (!foundUser) {
+      // User not found
+      return Response.json(
+        { success: false, message: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    // Return the user's message acceptance status
+    return Response.json(
+      {
+        success: true,
+        isAcceptingMessages: foundUser.isAcceptingMessage,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(
+      "Error retrieving message acceptance status:",
+      error
+    );
+    return Response.json(
+      {
+        success: false,
+        message: "Error retrieving message acceptance status",
+      },
+      { status: 500 }
+    );
+  }
+}
